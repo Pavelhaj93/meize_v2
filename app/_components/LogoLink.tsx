93ff0fa70b0e0021e68b5ccbc1de561a54674f5b
Pavelhaj93 <pavelhajduch93@gmail.com>
2024-11-29 12:24:50 +0100

    feat: logo link

--- app/_components/LogoLink.tsx
+++ app/_components/LogoLink.tsx
@@ -3,7 +3,7 @@ import SvgLogo from "./svg/SvgLogo";
 
 export default function LogoLink({ ...rest }) {
   return (
-    <Link href="/" {...rest}>
+    <Link href="#" {...rest}>
       <SvgLogo className="dark:fill-white h-20 w-24 dark:hover:text-gray-500 fill-black hover:fill-gray-500 transition-colors" />
     </Link>
   );
