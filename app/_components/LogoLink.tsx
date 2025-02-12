7dedabf63545c1cfa31f2f6361fbf225861c4067
Pavelhaj93 <pavelhajduch93@gmail.com>
2025-02-12 14:06:16 +0100

    feat: change logo

--- app/_components/LogoLink.tsx
+++ app/_components/LogoLink.tsx
@@ -3,19 +3,22 @@
 import { useLocale } from "next-intl";
 import Image from "next/image";
 import Link from "next/link";
-import SvgLogo from "./svg/SvgLogo";
 
-export default function LogoLink({ ...rest }) {
-	const locale = useLocale();
-	return (
-		<Link href={`/${locale}`} {...rest}>
-			{/* <SvgLogo className="dark:fill-white h-20 w-24 dark:hover:text-gray-500 fill-black hover:fill-gray-500 transition-colors" /> */}
-			<Image
-				src="/images/MEIZE_silver.png"
-				width={120}
-				height={120}
-				alt="Meize Logo"
-			/>
-		</Link>
-	);
+interface LogoLinkProps {
+  theme?: "black" | "white";
+}
+
+export default function LogoLink({ theme, ...props }) {
+  const locale = useLocale();
+  return (
+    <Link href={`/${locale}`} {...props}>
+      {/* <SvgLogo className="dark:fill-white h-20 w-24 dark:hover:text-gray-500 fill-black hover:fill-gray-500 transition-colors" /> */}
+      <Image
+        src={`/images/MEIZE_${theme === "black" ? "silver" : "white"}.png`}
+        width={120}
+        height={120}
+        alt="Meize Logo"
+      />
+    </Link>
+  );
 }
