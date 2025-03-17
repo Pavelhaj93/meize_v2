224bcc4fb4392d90d47810197377e95144fb81e5
Pavelhaj93 <pavelhajduch93@gmail.com>
2025-03-17 14:57:48 +0100

    feat: change padding on contact page

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -7,56 +7,56 @@ import { type Contacts, contacts } from "@/helpers/contacts";
 import { useTranslations } from "next-intl";
 
 export const metadata = {
-	title: "Contact",
-	description: "Contact page",
+  title: "Contact",
+  description: "Contact page",
 };
 
 export default function Contact() {
-	const t = useTranslations("contact");
+  const t = useTranslations("contact");
 
-	return (
-		<main className="bg-black dark min-h-screen">
-			<Nav theme="dark" />
+  return (
+    <main className="bg-black dark min-h-screen">
+      <Nav theme="dark" />
 
-			<Container first className="md:pt-20 flex flex-col xl:gap-28 gap-5">
-				<section className="w-full flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-0 max-w-[1000px] mx-auto">
-					<header className="flex-1">
-						<h2 className="font-medium tracking-tighter text-3xl lg:text-4xl xl:text-5xl dark:text-white">
-							{t("general")}
-						</h2>
-					</header>
-					<div className="flex-1 flex flex-col items-center lg:items-start gap-6 lg:gap-4 lg:pt-6 mt-5">
-						{Object.keys(contacts).map((type) => {
-							const contactType = type as keyof Contacts; // Explicitly cast to keyof Contacts
-							return (
-								<div key={`Contacts: ${contactType}`}>
-									<div className="inline-flex text-2xl dark:text-white mr-2">
-										{t(contactType)}:
-									</div>
-									<a
-										href={contacts[contactType].href}
-										className="inline-flex text-2xl text-black dark:text-white hover:text-white/50 transition-colors border-b border-transparent"
-									>
-										{contacts[contactType].title}
-									</a>
-								</div>
-							);
-						})}
-						<SocialIcons />
-					</div>
-				</section>
+      <Container first className="md:pt-20 pt-14 flex flex-col xl:gap-28 gap-5">
+        <section className="w-full flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-0 max-w-[1000px] mx-auto">
+          <header className="flex-1">
+            <h2 className="font-medium tracking-tighter text-3xl lg:text-4xl xl:text-5xl dark:text-white">
+              {t("general")}
+            </h2>
+          </header>
+          <div className="flex-1 flex flex-col items-center lg:items-start gap-6 lg:gap-4 lg:pt-6 mt-5">
+            {Object.keys(contacts).map((type) => {
+              const contactType = type as keyof Contacts; // Explicitly cast to keyof Contacts
+              return (
+                <div key={`Contacts: ${contactType}`}>
+                  <div className="inline-flex text-2xl dark:text-white mr-2">
+                    {t(contactType)}:
+                  </div>
+                  <a
+                    href={contacts[contactType].href}
+                    className="inline-flex text-2xl text-black dark:text-white hover:text-white/50 transition-colors border-b border-transparent"
+                  >
+                    {contacts[contactType].title}
+                  </a>
+                </div>
+              );
+            })}
+            <SocialIcons />
+          </div>
+        </section>
 
-				<section className="w-full flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-0 max-w-[1000px] mt-10 lg:mt-0 mx-auto mb-10">
-					<header className="flex-1">
-						<h3 className="font-medium tracking-tighter text-3xl lg:text-4xl xl:text-5xl dark:text-white">
-							{t("message")}
-						</h3>
-					</header>
-					<div className="flex-1 w-full max-w-[500px] lg:max-w-none">
-						<FormContact />
-					</div>
-				</section>
-			</Container>
-		</main>
-	);
+        <section className="w-full flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-0 max-w-[1000px] mt-10 lg:mt-0 mx-auto mb-10">
+          <header className="flex-1">
+            <h3 className="font-medium tracking-tighter text-3xl lg:text-4xl xl:text-5xl dark:text-white">
+              {t("message")}
+            </h3>
+          </header>
+          <div className="flex-1 w-full max-w-[500px] lg:max-w-none">
+            <FormContact />
+          </div>
+        </section>
+      </Container>
+    </main>
+  );
 }
--- app/[locale]/projects/page.tsx
+++ app/[locale]/projects/page.tsx
@@ -6,20 +6,20 @@ import { useTranslations } from "next-intl";
 import { useMemo } from "react";
 
 export default function ProjectsPage() {
-	const t = useTranslations("projects");
+  const t = useTranslations("projects");
 
-	const projects = useMemo(() => {
-		return getAllProjects();
-	}, []);
+  const projects = useMemo(() => {
+    return getAllProjects();
+  }, []);
 
-	return (
-		<div className="">
-			<Navbar />
-			<main className="max-md:pt-14">
-				<Container first>
-					<MasonryGallery items={projects} />
-				</Container>
-			</main>
-		</div>
-	);
+  return (
+    <div className="">
+      <Navbar />
+      <main className="max-md:pt-14">
+        <Container first>
+          <MasonryGallery items={projects} />
+        </Container>
+      </main>
+    </div>
+  );
 }
